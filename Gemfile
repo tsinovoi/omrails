cc64c90b3ca34a6a1b543c587cc6a5db566bb01a
Eli Tsinovoi <tsinovoi@gmail.com>
2013-03-08 18:11:28 -0500

    Add Bootstrap

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :assets do
   gem 'coffee-rails', '3.2.2'
 
   gem 'uglifier', '1.2.3'
+  gem "bootstrap-sass", "~> 2.3.0.1"
 end
 
 gem 'jquery-rails', '2.0.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Omrails::Application.routes.draw do
+  get 'about' => 'Pages#about'
+
+  root :to => 'Pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
